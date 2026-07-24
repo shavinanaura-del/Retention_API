240d67fbda5bff6058c827f76eecdf317120d662
Shavina Putri Naura <shavina.naura@gmail.com>
2026-07-24 13:53:36 +0700

    ubah predict jadi menerima input customer

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 import pandas as pd
 import joblib
 
@@ -7,8 +8,12 @@ app = FastAPI()
 # load model
 model = joblib.load("model_retention.pkl")
 
-# load data 2026
-data = pd.read_excel("2026 Clasify.xlsx")
+
+class Customer(BaseModel):
+    Segment_id: int
+    Usia: int
+    Closing: int
+    RSP: float
 
 
 @app.get("/")
@@ -18,26 +23,22 @@ def home():
     }
 
 
-@app.get("/customers")
-def customers():
-    return data.to_dict(orient="records")
+@app.post("/predict")
+def predict(customer: Customer):
+
+    # data dari Google Sheet masuk ke sini
+    X = pd.DataFrame([{
+        "Segment_id": customer.Segment_id,
+        "Usia": customer.Usia,
+        "Closing": customer.Closing,
+        "RSP": customer.RSP
+    }])
 
 
-@app.get("/predict")
-def predict():
+    prediction = model.predict(X)[0]
 
-    X = data[
-        [
-            "Segment_id",
-            "Usia",
-            "Closing",
-            "RSP"
-        ]
-    ]
 
-    prediction = model.predict(X)
-
-    hasil = data.copy()
-    hasil["Prediction"] = prediction
-
-    return hasil.to_dict(orient="records")
+    return {
+        "Prediction": int(prediction),
+        "Retention": "Balik" if prediction == 1 else "Tidak Balik"
+    }
